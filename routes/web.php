cdfbcc925dcff6e042c24736d688ae2fc0ebc0b2
Your Name <poria22.azami@gmail.com>
2024-11-23 15:44:08 +0330

    feat: telegram api

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TelegramBotController;
 
-Route::get('/', [\App\Http\Controllers\CrawlerController::class, 'index']);
+//Route::get('/', [\App\Http\Controllers\CrawlerController::class, 'index']);
+
+Route::post('/webhook', [TelegramBotController::class, 'handleWebhook']);
